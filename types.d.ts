535fbe13f3e434c532d3b04387bcc0e1d427e8cd
홍희림 <hhr8540@naver.com>
2022-03-24 22:02:19 +0900

    init: Webpack 설정
    * initial settings: webpack, babel, run helloworld
    
    * prettier, eslint setting
    
    * prettier, eslint setting

---
+++ types.d.ts
@@ -0,0 +1,8 @@
+declare module "*.jpeg" {
+  const src: string;
+  export default src;
+}
+declare module "*.svg" {
+  const src: string;
+  export default src;
+}
---
+++ webpack.dev.js
@@ -0,0 +1,12 @@
+const webpack = require('./webpack.common');
+const { merge } = require('webpack-merge');
+const path = require("path");
+
+module.exports = merge(webpack,
+  {
+    mode: "development",
+    devServer: {
+      hot: true,
+    },
+  }
+)
