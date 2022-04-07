99ab6bb8af93a477ef72ffed35daef4787e2cfd2
Henry-Hong <hhr8540@naver.com>
2022-04-07 13:29:16 +0900

    feature/#4 날씨API 수정완료

--- src/Forecast.tsx
+++ src/Forecast.tsx
@@ -6,39 +6,41 @@ function serializeDate(date: number): string {
   return date < 10 ? "0" + date : "" + date;
 }
 
-function getBaseDate(): string[] {
+function getBaseDate(): Date {
+  const currentDate = new Date();
+  const oneHourInMil = 3600000;
+  return new Date(currentDate.getTime() - ((currentDate.getHours() % 3) + 1) * oneHourInMil);
+}
+
+function getDateForWeatherAPI(): string[] {
   let base_date = "";
   let base_time = "";
 
-  // dateObj에 저장하기전, 00:00 ~ 01:59의 시간대는 없기때문에
-  // 날짜를 전날 23:00 기준으로 정해주어야한다.
-  // 날짜 계산을 먼저 하도록.
+  const baseDate = getBaseDate();
 
-  const dateObj = new Date();
-  const year = dateObj.getFullYear().toString();
-  const month = dateObj.getMonth() + 1; //JAN == 0
-  const day = dateObj.getDate();
-  const hour = dateObj.getHours();
+  const year = baseDate.getFullYear();
+  const month = baseDate.getMonth() + 1; //JAN == 0
+  const day = baseDate.getDate();
+  const hour = baseDate.getHours();
+  const minute = baseDate.getMinutes();
 
-  base_date += year;
+  base_date += serializeDate(year);
   base_date += serializeDate(month);
   base_date += serializeDate(day);
-  base_time += serializeDate(hour); //tobase_time
-  base_time += "00";
+  base_time += serializeDate(hour);
+  base_time += serializeDate(minute);
 
   return [base_date, base_time];
 }
 
-function getPosition(): string[] {
+function getPositionForWeatherAPI(): string[] {
   return ["55", "127"];
 }
 
 async function getWeatherInfos() {
-  const [base_date, base_time] = getBaseDate();
-  const [nx, ny] = getPosition();
-  console.log(base_date, base_time, nx, ny);
+  const [base_date, base_time] = getDateForWeatherAPI();
+  const [nx, ny] = getPositionForWeatherAPI();
   const address = `/api/1360000/VilageFcstInfoService_2.0/getVilageFcst` + `?serviceKey=${process.env.WEATHER_API_KEY}` + `&numOfRows=12` + `&pageNo=1` + `&base_date=${base_date}` + `&base_time=${base_time}` + `&nx=${nx}&ny=${ny}` + `&dataType=JSON`;
-  console.log(address);
 
   const response = await axios.get(address);
   console.log(response.data);
