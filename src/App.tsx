36cc2a495810ee5fa8b54e3c002d7bf594910e95
kimud6003 <kimud6003@gmail.com>
2022-04-08 16:22:27 +0900

    Weather Card Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React, { useState } from "react";
 import LocationManager from "./services/LocationManager";
+import WeatherCard from './components/WeatherCard'
+import { GlobalStyle } from './styles/Globalstyle';
+import { ThemeProvider } from 'styled-components';
+import { Mytheme } from './styles/theme';
 
 import p410 from "../public/assets/410.svg";
 import cat from "../public/assets/spacecat.jpeg";
+import Text from "./components/Text";
 
 function App() {
-  return <></>;
+  return(
+    <ThemeProvider theme={Mytheme}>
+      <GlobalStyle />
+      <></>
+    </ThemeProvider>
+  )
 }
 
 export default App;
